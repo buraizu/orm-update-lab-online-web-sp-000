2c1ca78c92eac172c0fce8bfafc0040b76fd2d56
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-01 03:24:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
     #   self.update
     # else
       sql = <<-SQL
-      INSERT INTO students 
+      INSERT INTO students
       VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
